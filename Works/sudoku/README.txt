23f58a1f453afe6df9cc0ec418827f135bf5052e
honglu <honglux@cd.uoregon.edu>
2016-06-11 03:55:19 -0700

    sudoku

---
+++ Works/sudoku/README.txt
@@ -0,0 +1,52 @@
+Sudoku
+------
+
+By: Honglu Xu
+
+----
+Info
+----
+
+A tool for solving sudoku problems
+
+---------------
+Compile and run
+---------------
+
+In the directory "sudoku", Use the command "python3 sudoku.py"
+
+---------------------------
+Executable file for windows
+---------------------------
+
+Unzip the file "sudoku_windows.zip".
+
+Excute the file "sudoku.exe".
+
+-----
+Usage
+-----
+
+Enter any key to close the program.
+
+-----
+Board
+-----
+
+Board file: "myboard.txt"
+
+'.' for the unknow number
+
+--------
+Citation
+--------
+
+'sdkboard.py': https://www.cs.uoregon.edu/Classes/13F/cis210/assignments/base/sudoku-helper/sdkboard.py
+
+'sdkdisplay.py': https://www.cs.uoregon.edu/Classes/13F/cis210/assignments/base/sudoku-helper/sdkdisplay.py
+
+'sdktextview.py': https://www.cs.uoregon.edu/Classes/13F/cis210/assignments/base/sudoku-helper/sdktextview.py
+
+'grid.py': https://www.cs.uoregon.edu/Classes/13F/cis210/assignments/base/sudoku-helper/grid.py
+
+'graphics.py': https://www.cs.uoregon.edu/Classes/13F/cis210/assignments/base/sudoku-helper/graphics.py
---
+++ Works/sudoku/sudoku/sudoku.py
@@ -0,0 +1,73 @@
+
+"""
+Sudoku solver.
+
+Overall application control (including instantiation of 
+graphical and/or textual views of progress) and overall 
+strategy (repeated application of tactics from cheapest
+to most expensive). 
+
+Author: Michal Young, 10-15 November 2012 for CIS 210
+
+"""
+import sdkboard    # The Sudoku board  (model component)
+import sdkdisplay  # Display of Sudoku board (view component)
+import sdktactics  # Tactics for checking and solving sudoku
+import sdktextview # Alternative display: spew text
+import argparse    # Interpret command line
+
+'''
+###
+# Command line processing - the Pythonic way
+###
+
+parser = argparse.ArgumentParser(description="""
+   Check a Sudoku board (complete or incomplete) to
+   report whether it is correct or whether, on the
+   contrary, there are duplicate digits in a row,
+   a column, or a subsquare.  If there are no 
+   errors, proceed to fill in as many open tiles
+   as possible using the naked single and hidden 
+   single tactics. """)
+
+parser.add_argument('board', metavar="filename",
+                    type=argparse.FileType('r'), 
+                    help="file containing board description")
+                    
+parser.add_argument('--display', action="store_true", 
+                    help="Display Sudoku board graphically")
+                    
+parser.add_argument('--text', action="store_true", 
+                    help="Textual display to standard output")
+                    
+
+args = parser.parse_args()
+'''
+###
+# Main program logic
+###
+
+f = "myboard.txt"
+try:
+	board_f = open(f,'r')
+except:
+	print("Board file error!")
+	input()
+	exit(1)
+board = sdkboard.Board.from_file(board_f)
+
+sdkdisplay.display(board)
+
+sdktextview.display(board)
+
+sdktactics.prepare(board)
+if sdktactics.good_board():
+	sdktactics.solve()
+	print(board)
+else:
+    print("Sudoku FAIL")
+
+input("Press enter to close down")
+
+
+
